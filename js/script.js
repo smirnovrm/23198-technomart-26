97affa445cfa9e2b813a0e983dbb4fc1255cf23e
Roman Smirnov <r.smirnov86@gmail.com>
2019-10-26 21:07:15 +0600

    Добавлены стили для модального окна Добавления товара в корзину

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
 var mapModal = document.querySelector("#map-modal")
 var contactModal = document.querySelector("#contact-modal")
-
-var closeMapBtn = document.querySelector("#map-modal .popup-close")
-var closeContactBtn = document.querySelector("#contact-modal .popup-close")
+// var basketModal = document.querySelector("#basket-modal")
 
 var openMap = document.querySelector("#map")
 var openContact = document.querySelector("#open-contact")
+// var openBasket = document.querySelectorAll(".buy");
 
-openMap.addEventListener("click", function(event){
+// var buyBtn = document.querySelector("#buy-btn")
+// var cntBtn = document.querySelector("#continue-btn")
+// console.log(openBasket);
 
-  mapModal.classList.add("show")
+var closeMapBtn = document.querySelector("#map-modal .popup-close")
+var closeContactBtn = document.querySelector("#contact-modal .popup-close")
+// var closeBasketBtn = document.querySelector("#basket-modal .modal-wrap .popup-close")
 
-})
-closeMapBtn.addEventListener("click", function(event){
+try {
+  openMap.addEventListener("click", function(event){
 
-  mapModal.classList.remove("show")
+    mapModal.classList.add("show")
 
-})
+  })
+  closeMapBtn.addEventListener("click", function(event){
 
-openContact.addEventListener("click", function(event){
+    mapModal.classList.remove("show")
 
-  event.preventDefault
-  contactModal.classList.add("show")
-
-})
-
-closeContactBtn.addEventListener("click", function(event){
-
-  contactModal.classList.remove("show")
-
-})
+  })
+} catch (err) {
+  console.log("Не найдено модальное окно с картой")
+}
 
 
-// console.log(openContactBtn)
-// console.log(mapModal);
-// console.log(closeBtn);
+try {
+  openContact.addEventListener("click", function(event){
+
+    event.preventDefault
+    contactModal.classList.add("show")
+
+  })
+  closeContactBtn.addEventListener("click", function(event){
+
+    contactModal.classList.remove("show")
+
+  })
+} catch (err) {
+  console.log("На странице отсутствует модальное окно с контактной формой")
+}
